d00047d59b432b8d1ce08a65801a2131f98f833c
sjaiz1717 <137878087+sjaiz1717@users.noreply.github.com>
2023-06-30 12:47:59 +0900

    [Gold IV] Title: 타임머신, Time: 196 ms, Memory: 115152 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -3,14 +3,6 @@ from sys import stdin
 input = stdin.readline
 INF = int(1e9)
 
-# 입력
-N, M = map(int, input().split())
-graph = []
-distance = [INF] * (N+1)
-for _ in range(M):
-    A, B, C = map(int, input().split())
-    graph.append((A,B,C))
-
 # 벨만포드 알고리즘
 def BellmanFord(start):
     distance[start] = 0
@@ -22,6 +14,14 @@ def BellmanFord(start):
                 if i == N-1:
                     return True
     return False
+
+# 입력
+N, M = map(int, input().split())
+graph = []
+distance = [INF] * (N+1)
+for _ in range(M):
+    A, B, C = map(int, input().split())
+    graph.append((A,B,C))
 
 # 결과 출력
 negative_cycle = BellmanFord(1)
